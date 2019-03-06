91947f5c695dbcdf0831980fce108ff35e9ca454
Adam <adam@Adams-MacBook-Air.local>
2019-03-06 15:53:39 +0000

    Updates

---
+++ precache-manifest.e84f7668f990eee66e2aa03de9ea5189.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cads-finder/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4f998a2577bc82aeb13b",
+    "url": "/cads-finder/static/js/runtime~main.4f998a25.js"
+  },
+  {
+    "revision": "ae22ed7f9f4aafbf87b7",
+    "url": "/cads-finder/static/js/main.ae22ed7f.chunk.js"
+  },
+  {
+    "revision": "fb84b4754607eb9ca2dc",
+    "url": "/cads-finder/static/js/2.fb84b475.chunk.js"
+  },
+  {
+    "revision": "ae22ed7f9f4aafbf87b7",
+    "url": "/cads-finder/static/css/main.84872f57.chunk.css"
+  },
+  {
+    "revision": "fb84b4754607eb9ca2dc",
+    "url": "/cads-finder/static/css/2.48ba4ddf.chunk.css"
+  },
+  {
+    "revision": "21068aefe780444d3a0840b3ede82a17",
+    "url": "/cads-finder/index.html"
+  }
+];
